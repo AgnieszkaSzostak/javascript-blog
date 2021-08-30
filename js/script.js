748b20f5d8d185bcc064040a190b8bc716689430
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-08-31 00:19:55 +0200

    Add loop for articles

--- js/script.js
+++ js/script.js
@@ -1,42 +1,14 @@
 'use strict';
 
-
-// po kliknięciu linka:
-
-//    ustaw klasy linków:
-//      usuń klasę active ze wszystkich linków na liście tytułów,
-//      dodaj klasę active do klikniętego linka,
-
-//  ukryj wszystkie artykuły:
-//      usuń klasę active ze wszystkich artykułów,
-
-//  znajdź artykuł do wyświetlenia:
-//      z klikniętego linka weź zawartość atrybutu href, np. #article-2,
-//      znajdź na stronie element pasujący do selektora takiego, jak wartość atrybutu href, np. #article-2 – czyli   szukamy elementu o id="article-2",
-
-//  wyświetl znaleziony artykuł:
-//      dodaj klasę active do znalezionego artykułu.
-
-// function titleClickHandler(){
-//   const links = document.querySelectorAll('.titles a');
-//   console.log(links);
-// }
-//
-// const buttonTest = document.getElementById('button-test');
-//
-// buttonTest.addEventListener('click', titleClickHandler);
-
 /*Function Handler event click */
 
 function titleClickHandler(event){
   event.preventDefault();
-
   const clickedElement = this;
   console.log('Link was clicked!');
 
     /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
   }
@@ -49,22 +21,21 @@ function titleClickHandler(event){
   for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
   }
-
     /* get 'href' attribute from the clicked link */
+
   const hrefAttribute = clickedElement.getAttribute('href');
   // hrefAttribute = hrefAttribute.substr(1);
   console.log('hrefAttribute:', hrefAttribute);
-    // znajdź artykuł do wyświetlenia:
-   //      z klikniętego linka weź zawartość atrybutu href, np. #article-2,
-   //      znajdź na stronie element pasujący do selektora takiego, jak wartość atrybutu href, np. #article-2 – czyli   szukamy elementu o id="article-2",
-
     /* find the correct article using the selector (value of 'href' attribute) */
-    let article = document.querySelector(hrefAttribute);
-    console.log('article:', article);
+
+  let article = document.querySelector(hrefAttribute);
+  console.log('article:', article);
     // let article = document.getElementById(hrefAttribute);
     // console.log(article);
+
     // /* add class 'active' to the correct article */
-    article.classList.add('active');
+
+  article.classList.add('active');
 }
 /* Function clearLinks */
 
@@ -76,22 +47,25 @@ function clearLinks(){
   }
 }
 
+/* Function generate list of titles */
+
 function generateTitleLinks(){
   console.log('funkcja generateTitleLinks została wywołana');
 
 
 //   następnie dla każdego artykułu:
 // odczytaj jego id i zapisz je do stałej,
+// znajdź element z tytułem i zapisz jego zawartość do stałej,
   let listArticles = document.querySelectorAll('.post');
   console.log('listArticles:', listArticles);
   for(let article of listArticles) {
     const articleId = article.getAttribute('id');
-    const articleTitle = article.firstChild;
+    const articleTitle = article.firstElementChild.textContent;
+    const articleLink = '<li><a href="#' + articleId + '><span>' + articleTitle + '</span></a></li>';
+    console.log('articleLink =',  articleLink);
     console.log('articleId is:', articleId, 'articleTitle:', articleTitle);
 
   }
-
-// znajdź element z tytułem i zapisz jego zawartość do stałej,
 
 // na podstawie tych informacji stwórz kod HTML linka i zapisz go do stałej,
 // wstaw stworzony kod HTML do listy linków w lewej kolumnie.
