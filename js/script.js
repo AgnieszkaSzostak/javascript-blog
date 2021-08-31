a5bf0d440a4c9f0857955fda841c017c19bd24b4
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-08-31 15:45:39 +0200

    Fix script

--- js/script.js
+++ js/script.js
@@ -33,50 +33,50 @@ function titleClickHandler(event){
     // let article = document.getElementById(hrefAttribute);
     // console.log(article);
 
-    // /* add class 'active' to the correct article */
-
+  // /* add class 'active' to the correct article */
   article.classList.add('active');
 }
 /* Function clearLinks */
 
 function clearLinks(){
-  let links2Clear = document.querySelectorAll('.titles li');
+
+  let links2Clear = document.querySelectorAll('.titles > li');
   console.log('links2Clear', links2Clear);
   for(let link of links2Clear){
       link.innerHTML = '';
   }
 }
+/* ... */
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
 
 /* Function generate list of titles */
 
 function generateTitleLinks(){
   console.log('funkcja generateTitleLinks została wywołana');
-
-
-//   następnie dla każdego artykułu:
-// odczytaj jego id i zapisz je do stałej,
-// znajdź element z tytułem i zapisz jego zawartość do stałej,
-  let listArticles = document.querySelectorAll('.post');
+  let listArticles = document.querySelectorAll(optArticleSelector);
   console.log('listArticles:', listArticles);
   for(let article of listArticles) {
     const articleId = article.getAttribute('id');
-    const articleTitle = article.firstElementChild.textContent;
-    const articleLink = '<li><a href="#' + articleId + '><span>' + articleTitle + '</span></a></li>';
+    // const articleTitle = article.firstElementChild.textContent;
+    console.log('articleId is:', articleId)
+    const articleTitle = article.querySelector(optTitleSelector);
+    console.log('articleTitle:', articleTitle);
+    const articleLink = '<li><a href="#' + articleId + '><span>' + articleTitle.textContent + '</span></a></li>';
     console.log('articleLink =',  articleLink);
-    console.log('articleId is:', articleId, 'articleTitle:', articleTitle);
-
+    let titleList = document.querySelector('.titles');
+    console.log('titleList:', titleList);
+    titleList.insertAdjacentElement('afterbegin', articleLink);
   }
-
-// na podstawie tych informacji stwórz kod HTML linka i zapisz go do stałej,
-// wstaw stworzony kod HTML do listy linków w lewej kolumnie.
-
-
 }
-
+//
 clearLinks();
 generateTitleLinks();
-const links = document.querySelectorAll('.titles a');
-console.log(links);
+const links = document.querySelectorAll('.titles li');
+console.log('links:', links);
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
