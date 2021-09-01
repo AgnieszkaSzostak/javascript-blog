b19db3f887d7f978b29fdf721dd5db2ccc838f8e
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-09-01 14:28:58 +0200

    Fix titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    const activeLinks = document.querySelectorAll('.titles li .active ');
-    for(let activeLink of activeLinks){
+  event.preventDefault();
+  const clickedElement = this;
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
-    }
-    clickedElement.classList.add('active');
-    const activeArticles = document.querySelectorAll('.posts .post.active');
-    for(let activeArticle of activeArticles){
-        activeArticle.classList.remove('active');
-    }
-    const hrefAttribute = clickedElement.getAttribute('href');
-    let article = document.querySelector(hrefAttribute);
-    article.classList.add('active');
+  }
+  clickedElement.classList.add('active');
+  const activeArticles = document.querySelectorAll('.posts .post.active');
+  for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+  }
+  const hrefAttribute = clickedElement.getAttribute('href');
+  let article = document.querySelector(hrefAttribute);
+  article.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles li');
