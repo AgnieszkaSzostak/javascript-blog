0025c89645b4ad68eb854cc0791579909ea93bea
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-09-01 14:32:29 +0200

    Fix function

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 'use strict';
 
 function titleClickHandler(event){
-  event.preventDefault();
-  const clickedElement = this;
-  const activeLinks = document.querySelectorAll('.titles a.active');
-  for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
-  }
-  clickedElement.classList.add('active');
-  const activeArticles = document.querySelectorAll('.posts .post.active');
-  for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-  }
-  const hrefAttribute = clickedElement.getAttribute('href');
-  let article = document.querySelector(hrefAttribute);
-  article.classList.add('active');
+    event.preventDefault();
+    const clickedElement = this;
+    const activeLinks = document.querySelectorAll('.titles a.active');
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+    clickedElement.classList.add('active');
+    const activeArticles = document.querySelectorAll('.posts .post.active');
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+    const hrefAttribute = clickedElement.getAttribute('href');
+    let article = document.querySelector(hrefAttribute);
+    article.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles li');
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
 }
