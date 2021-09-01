8bed5ea2b5a8978a5d27c03abf19dbcb61744e67
Agnieszka Szostak <aa.szostak3@student.uw.edu.pl>
2021-09-01 12:07:27 +0200

    Fix titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -6,10 +6,8 @@ function titleClickHandler(event){
   const clickedElement = this;
   console.log('Link was clicked!');
 
-
     /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
-
   for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
   }
@@ -18,18 +16,23 @@ function titleClickHandler(event){
   clickedElement.classList.add('active');
       /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts .post.active');
-  console.log('aktywne linki to: ');
-  console.log(activeArticles);
+  console.log('aktywny artykuł to: ', activeArticles);
   for(let activeArticle of activeArticles){
       activeArticle.classList.remove('active');
   }
-
     /* get 'href' attribute from the clicked link */
 
-
+  const hrefAttribute = clickedElement.getAttribute('href');
+  // hrefAttribute = hrefAttribute.substr(1);
+  console.log('hrefAttribute:', hrefAttribute);
     /* find the correct article using the selector (value of 'href' attribute) */
 
-    /* add class 'active' to the correct article */
+  let article = document.querySelector(hrefAttribute);
+  console.log('article:', article);
+    // let article = document.getElementById(hrefAttribute);
+    // console.log(article);
+  // /* add class 'active' to the correct article */
+  article.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
